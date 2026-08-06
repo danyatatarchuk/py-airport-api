bed490173b191cc623e148dc9a772175cca9c01f
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-06 22:43:06 +0300

    Add crew model

--- airport/models.py
+++ airport/models.py
@@ -42,3 +42,11 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.source} -> {self.destination}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
