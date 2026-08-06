d2c5928c9065a236411f57d30c2a87113b829557
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-07 01:42:22 +0300

    Add airplane endpoints

--- airport/urls.py
+++ airport/urls.py
@@ -3,18 +3,13 @@ from rest_framework.routers import DefaultRouter
 from airport.views import (
     AirportViewSet,
     AirplaneTypeViewSet,
+    AirplaneViewSet,
 )
 
 router = DefaultRouter()
 
-router.register(
-    "airports",
-    AirportViewSet,
-)
-
-router.register(
-    "airplane-types",
-    AirplaneTypeViewSet,
-)
+router.register("airports", AirportViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
 
 urlpatterns = router.urls
--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
 
-from airport.models import Airport, AirplaneType
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+)
 from airport.serializers import (
     AirportSerializer,
+    AirplaneSerializer,
     AirplaneTypeSerializer,
 )
+
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
@@ -14,3 +20,8 @@ class AirportViewSet(viewsets.ModelViewSet):
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
