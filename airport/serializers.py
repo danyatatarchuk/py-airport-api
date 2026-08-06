c2d883419f2ac9a072fddfeacd624342d3ff2493
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-07 00:31:23 +0300

    Add airport and airplane type serializers

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,1 +1,24 @@
 from rest_framework import serializers
+
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+    Crew,
+    Flight,
+    Order,
+    Route,
+    Ticket,
+)
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = "__all__"
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = "__all__"
