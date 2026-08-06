ba73242a955504bac89b6e7121a2ea84cd1bf36c
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-06 23:01:01 +0300

    Add order model

--- airport/models.py
+++ airport/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Airport(models.Model):
@@ -75,3 +76,15 @@ class Flight(models.Model):
             f"{self.route} | "
             f"{self.departure_time.strftime('%Y-%m-%d %H:%M')}"
         )
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+    def __str__(self):
+        return f"Order #{self.id}"
