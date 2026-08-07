35aa24eba521d4e8e5fcd767b3c838b25bd3baf3
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-07 16:25:25 +0300

    Add JWT authentication

--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path("api/airport/", include("airport.urls")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
@@ -31,5 +37,14 @@ urlpatterns = [
         name="swagger-ui",
     ),
 
-    path("api/airport/", include("airport.urls")),
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 ]
